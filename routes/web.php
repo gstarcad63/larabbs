1d1b869785412091b3a4cbca36923d4bd6f0fd77
gstarcad <358754733@qq.com>
2020-12-03 06:02:26 +0000

    翻译 URL 标示

--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,12 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
 // 话题
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 // 上传图片
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
+// URL翻译-SEO
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
